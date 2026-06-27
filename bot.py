3e9098882693eae6784c2e0dd0c32cce132ea0c6
Ashkannngufyuujgi <ashkanzlqy95@gmail.com>
2026-06-28 02:38:44 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,26 +2,27 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = os.getenv("8897799054:AAGLwUGSh_X3zkM1S9dhmAhpP4gWz2wQTm0")
-ADMIN_ID = int(os.getenv("8075183730"))
+# گرفتن اطلاعات از Environment Variables
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 user_map = {}
 next_id = 1000
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global next_id
-
     uid = update.effective_user.id
 
     if uid not in user_map:
         user_map[uid] = next_id
         next_id += 1
 
-    await update.message.reply_text("👋 پیامتو بفرست، کاملاً ناشناس میره برای ادمین.")
+    await update.message.reply_text("ربات فعال شد 👍 پیام شما ناشناس ارسال میشه")
+
 
 async def handle_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global next_id
-
     uid = update.effective_user.id
     text = update.message.text
 
@@ -32,12 +33,10 @@ async def handle_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
     code = user_map[uid]
 
     await context.bot.send_message(
-        ADMIN_ID,
-        f"📩 پیام جدید\n\n"
-        f"کد کاربر: {code}\n\n"
-        f"{text}\n\n"
-        f"↩ جواب بده:\n/reply {code} متن پاسخ"
+        chat_id=ADMIN_ID,
+        text=f"📩 پیام جدید\nکد کاربر: {code}\n\n{text}\n\nبرای پاسخ:\n/reply {code} جواب"
     )
+
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id != ADMIN_ID:
@@ -47,7 +46,7 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
         parts = update.message.text.split(maxsplit=2)
 
         if len(parts) < 3:
-            await update.message.reply_text("فرمت درست: /reply کد پیام جواب")
+            await update.message.reply_text("فرمت درست: /reply کد جواب")
             return
 
         code = int(parts[1])
@@ -63,10 +62,11 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.send_message(target, f"📨 پاسخ:\n{msg}")
             await update.message.reply_text("ارسال شد ✔️")
         else:
-            await update.message.reply_text("کاربر پیدا نشد ❌")
+            await update.message.reply_text("کاربر پیدا نشد")
 
     except:
-        await update.message.reply_text("خطا در فرمت دستور")
+        await update.message.reply_text("خطا در دستور")
+
 
 app = Application.builder().token(TOKEN).build()
 
